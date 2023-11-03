dc01ed4aff59e2738dd4993b66cd0a907ec757d5
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-11-03 08:35:02 +0100

    feat: create result component

---
+++ client/src/app/components/result/result.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { PhrasesService } from 'src/app/services/phrases.service';
+import { ResultService } from 'src/app/services/result.service';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.scss']
+})
+export class ResultComponent {
+  public score$ = this.resultService.score$;
+  public total = this.phrasesService.phrasesAmount$;
+
+  constructor (
+    private resultService: ResultService,
+    private phrasesService: PhrasesService
+  ) {}
+}
