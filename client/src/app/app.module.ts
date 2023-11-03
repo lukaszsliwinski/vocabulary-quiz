4f5a41ffe305daa0aff2dd4f33266a97863a3209
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-11-03 08:35:28 +0100

    feat: create card component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/card/card.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IPhrase } from 'src/app/models/phrase';
+import { PhrasesService } from 'src/app/services/phrases.service';
+import { ResultService } from 'src/app/services/result.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  private router =  inject(Router);
+
+  public nextDisabled: boolean = true;
+
+  public id: number = 1;
+  public phrase: IPhrase | undefined;
+  public plPhrase: string;
+  public translations: string[];
+  public categories: string[];
+  public correct: boolean | undefined;
+  public total: number = 0;
+
+  public answerForm = new FormGroup({
+    answerInput: new FormControl()
+  });
+
+  constructor(
+    private phrasesService: PhrasesService,
+    private resultService: ResultService
+  ) {}
+
+  ngOnInit() {
+    this.clearData();
+    this.getData()
+  }
+
+  clearData(): void {
+    this.phrase = undefined;
+    this.plPhrase = '';
+    this.translations = [];
+    this.categories = [];
+    this.correct = undefined;
+
+    this.answerForm.enable();
+    this.answerForm.reset();
+  }
+
+  getData(): void {
+    this.phrase = this.phrasesService.getPhraseById(this.id);
+    console.log(this.phrase);
+    this.phrasesService.phrasesAmount$.subscribe((total) => {
+      this.total = total;
+    })
+
+    if (this.phrase) {
+      this.plPhrase = this.phrase.pl;
+      this.phrase.en.forEach((engPhrase) => {
+        this.translations.push(engPhrase);
+      })
+      this.phrase.categories.forEach((category) => {
+        this.categories.push(category);
+      })
+    } else if (this.id > this.total) {
+      alert('handle end of quiz');
+      this.router.navigate(['/']);
+    } else {
+      // rozbudować
+      console.log('error');
+    }
+  }
+
+  check(): void {
+    const input = this.answerForm.value.answerInput;
+    if (typeof input === 'string') {
+      this.correct = this.phrasesService.checkTheCorrectness(input, this.translations);
+      this.nextDisabled = false;
+    };
+
+    if (this.correct !== undefined) this.answerForm.disable();
+    if (this.correct === true) this.resultService.incrementScore();
+
+  }
+
+  next(): void {
+    this.id = this.id + 1;
+    this.clearData();
+    this.getData();
+    this.nextDisabled = true;
+  }
+}
