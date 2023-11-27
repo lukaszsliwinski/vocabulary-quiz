363d886478273a595a11dd4af4c7fdab7e0c54b0
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-11-27 21:20:17 +0100

    feat: style categories menu

--- client/src/app/components/category/category.component.ts
+++ client/src/app/components/category/category.component.ts
@@ -4,25 +4,64 @@ import { PhrasesService } from 'src/app/services/phrases.service';
 import { ICategory } from 'src/app/models/category';
 import { ResultService } from 'src/app/services/result.service';
 
+import {
+  faHeartPulse,
+  faMountain,
+  faBriefcase,
+  faGears,
+  faFaceSmile,
+  faBurger,
+  faClipboardQuestion,
+  faCommentDots,
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
   private router = inject(Router);
 
   @Input() public category: ICategory;
+  @Input() public background: string;
+
+  public icons = [
+    {name: 'medicine', icon: faHeartPulse},
+    {name: 'nature', icon: faMountain},
+    {name: 'business', icon: faBriefcase},
+    {name: 'technical', icon: faGears},
+    {name: 'behaviour', icon: faFaceSmile},
+    {name: 'cooking', icon: faBurger},
+    {name: 'other', icon: faClipboardQuestion},
+    {name: 'verbs', icon: faCommentDots},
+    {name: 'phrases', icon: faCommentDots},
+    {name: 'nouns', icon: faCommentDots},
+    {name: 'adjectives', icon: faCommentDots},
+    {name: 'phrasal verbs', icon: faCommentDots}
+  ]
+
+  public icon: IconDefinition;
 
   constructor(
     private phrasesService: PhrasesService,
     private resultService: ResultService
   ) { }
 
+  ngOnInit(): void {
+    const faData = this.icons.find(obj => obj.name === this.category._id);
+    if (faData) this.icon = faData.icon;
+  }
+
   start(category: string): void {
     this.resultService.clearScore();
     this.phrasesService.getPhrases(category).subscribe(() => {
       this.router.navigate(['card'], { skipLocationChange: true });
     });
+  }
+
+  getBgUrl() {
+    return `url(./backgrounds/${this.category})`;
   }
 }
