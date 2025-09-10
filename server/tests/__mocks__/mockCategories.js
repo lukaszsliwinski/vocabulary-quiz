64c90aaa52adefada36b0f2d3d0053307e4d1b05
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-09-10 11:52:43 +0200

    test: create mock data for testing

---
+++ server/tests/__mocks__/mockCategories.js
@@ -0,0 +1,19 @@
+// Mock categories response from database
+
+const mockCategories = [
+  { _id: 'phrases', count: 64 },
+  { _id: 'nature', count: 90 },
+  { _id: 'technical', count: 54 },
+  { _id: 'idioms', count: 28 },
+  { _id: 'verbs', count: 72 },
+  { _id: 'behaviour', count: 160 },
+  { _id: 'nouns', count: 326 },
+  { _id: 'adjectives', count: 157 },
+  { _id: 'business', count: 68 },
+  { _id: 'phrasal verbs', count: 35 },
+  { _id: 'other', count: 206 },
+  { _id: 'cooking', count: 54 },
+  { _id: 'medicine', count: 50 }
+]
+
+module.exports = mockCategories;
---
+++ server/tests/__mocks__/mockPhrases.js
@@ -0,0 +1,76 @@
+// Mock phrases response from database
+
+const mockPhrases = [
+  {
+    _id: '662175062f4c2cf799d4695b',
+    date: '2024-03-28',
+    pl: 'lakierować',
+    en: [ 'varnish' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d46862',
+    date: '2023-03-02',
+    pl: 'zaklinowany',
+    en: [ 'jammed', 'stucked' ],
+    categories: [ 'adjectives', 'technical' ]
+  },
+  {
+    _id: '66d9cf2549b7275ed6285699',
+    date: '2024-09-05',
+    pl: 'słup energetyczny',
+    en: [ 'pylon' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d46851',
+    date: '2023-03-02',
+    pl: 'imbus',
+    en: [ 'allan key' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d467bf',
+    date: '2022-09-08',
+    pl: 'rowek',
+    en: [ 'groove' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d467de',
+    date: '2022-10-27',
+    pl: 'miedź',
+    en: [ 'copper' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d468f3',
+    date: '2023-09-21',
+    pl: 'asfalt',
+    en: [ 'tarmac' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d467cc',
+    date: '2022-09-29',
+    pl: 'opony',
+    en: [ 'tyres' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '667daf7c637ef683c50c4352',
+    date: '2024-06-27',
+    pl: 'gniazdko',
+    en: [ 'socket', 'outlet' ],
+    categories: [ 'nouns', 'technical' ]
+  },
+  {
+    _id: '662175062f4c2cf799d467dd',
+    date: '2022-10-27',
+    pl: 'cyna',
+    en: [ 'tin' ],
+    categories: [ 'nouns', 'technical' ]
+  }
+]
+
+module.exports = mockPhrases;
