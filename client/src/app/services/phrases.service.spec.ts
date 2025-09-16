476c0c7e2aa9643279020e94302b950acce11034
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-09-16 12:55:17 +0200

    test: create Phrases and Result services unit tests

--- client/src/app/services/phrases.service.spec.ts
+++ client/src/app/services/phrases.service.spec.ts
@@ -1,16 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+// Phrases service unit test
 
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { PhrasesService } from './phrases.service';
+import { IPhrase } from '../models/phrase';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PhrasesService', () => {
   let service: PhrasesService;
 
+  const mockPhrases: IPhrase[] = [
+    { id: 1, pl: 'lakierować', en: ['varnish'], categories: ['nouns', 'technical'] },
+    { id: 2, pl: 'zaklinowany', en: ['jammed', 'stucked'], categories: ['adjectives', 'technical'] },
+    { id: 3, pl: 'słup energetyczny', en: ['pylon'], categories: ['nouns', 'technical'] },
+    { id: 4, pl: 'imbus', en: ['allan key'], categories: ['nouns', 'technical'] },
+    { id: 5, pl: 'rowek', en: ['groove'], categories: ['nouns', 'technical'] },
+    { id: 6, pl: 'miedź', en: ['copper'], categories: ['nouns', 'technical'] },
+    { id: 7, pl: 'asfalt', en: ['tarmac'], categories: ['nouns', 'technical'] },
+    { id: 8, pl: 'opony', en: ['tyres'], categories: ['nouns', 'technical'] },
+    { id: 9, pl: 'gniazdko', en: ['socket', 'outlet'], categories: ['nouns', 'technical'] },
+    { id: 10, pl: 'cyna', en: ['tin'], categories: ['nouns', 'technical'] },
+  ];
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [PhrasesService,        
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
+
     service = TestBed.inject(PhrasesService);
+    (service as any).randomPhrases.next(mockPhrases);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get phrase by id', () => {
+    const phrase = service.getPhraseById(6);
+    expect(phrase).toBeDefined();
+  });
+
+  it('should correctly check input', () => {
+    expect(service.checkTheCorrectness('copper', mockPhrases.find(phrase => phrase.id === 6)!.en)).toBeTrue();
+    expect(service.checkTheCorrectness('COPPER', mockPhrases.find(phrase => phrase.id === 6)!.en)).toBeTrue();
   });
 });
--- client/src/app/services/result.service.spec.ts
+++ client/src/app/services/result.service.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+// Result service unit test
 
+import { TestBed } from '@angular/core/testing';
 import { ResultService } from './result.service';
+import { Router } from '@angular/router';
 
 describe('ResultService', () => {
   let service: ResultService;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        ResultService,
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
     service = TestBed.inject(ResultService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should increment score', () => {
+    expect(service.getScore()).toBe(0);
+    service.incrementScore();
+    expect(service.getScore()).toBe(1);
+  });
+
+  it('should clear score', () => {
+    service.incrementScore();
+    expect(service.getScore()).toBe(1);
+    service.clearScore();
+    expect(service.getScore()).toBe(0);
+  });
+
+  it('should open modal', (done) => {
+    service.isOpen$.subscribe(isOpen => {
+      if (isOpen) {
+        expect(isOpen).toBeTrue();
+        done();
+      }
+    });
+    service.openModal();
+  });
+
+  it('should close modal and navigate', () => {
+    service.closeModal();
+    service.isOpen$.subscribe(isOpen => expect(isOpen).toBeFalse());
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
   });
 });
