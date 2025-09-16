cffb5b4332c7d50e1b9a6b76bd6d9533f5bbb9b3
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-09-16 12:33:39 +0200

    test: create Category component unit test

--- client/src/app/components/category/category.component.spec.ts
+++ client/src/app/components/category/category.component.spec.ts
@@ -1,21 +1,43 @@
+// Category component unit test
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CategoryComponent } from './category.component';
+import { PhrasesService } from 'src/app/services/phrases.service';
+import { ICategory } from 'src/app/models/category';
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent;
   let fixture: ComponentFixture<CategoryComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CategoryComponent]
-    });
+  const mockCategory: ICategory = { _id: 'technical', count: 10 };
+
+  beforeEach(async () => {
+    // create test environment
+    const phrasesSpy = jasmine.createSpyObj('PhrasesService', ['getPhrases']);
+
+    await TestBed.configureTestingModule({
+      imports: [FontAwesomeModule],
+      declarations: [CategoryComponent],
+      providers: [
+        { provide: PhrasesService, useValue: phrasesSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CategoryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    component.category = mockCategory;
+    component.background = 'image';
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should assign correct icon', () => {
+    component.ngOnInit();
+    expect(component.icon).toBeDefined();
   });
 });
